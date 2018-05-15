e807f28a8225d57c7f293871d9a3a2e898fdd646
Алексей Ефимов <lex.efimov@gmail.com>
2018-05-15 10:04:33 +0500

    UITextView

--- UIElements/ViewController.swift
+++ UIElements/ViewController.swift
@@ -9,111 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var doneButton: UIButton!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var switchLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        slider.value = 1
-        
-        label.text = String(slider.value)
-        label.font = label.font.withSize(35)
-        label.textAlignment = .center
-        label.numberOfLines = 2
-        
-        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: true)
-        
-        slider.minimumValue = 0
-        slider.maximumValue = 1
-        slider.minimumTrackTintColor = .yellow
-        slider.maximumTrackTintColor = .red
-        slider.thumbTintColor = .blue
-        
-        datePicker.locale = Locale(identifier: "ru_RU")
-    }
-
-    @IBAction func choiceSegment(_ sender: UISegmentedControl) {
-        
-        label.isHidden = false
-        
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            label.text = "The first segment is selected"
-            label.textColor = .red
-        case 1:
-            label.text = "The second segment is selected"
-            label.textColor = .blue
-        case 2:
-            label.text = "The third segment is selected"
-            label.textColor = .yellow
-        default:
-            print("Sothing wrong!")
-        }
     }
     
-    @IBAction func sliderAction(_ sender: UISlider) {
-        label.text = String(sender.value)
-        
-        let backgrandColor = self.view.backgroundColor
-        self.view.backgroundColor = backgrandColor?.withAlphaComponent(CGFloat(sender.value))
-    }
-    
-    
-    @IBAction func donePressed(_ sender: UIButton) {
-        
-        guard textField.text?.isEmpty == false else { return }
-        
-        if let _ = Double(textField.text!) {
-            
-            let alert = UIAlertController(title: "Wrong format", message: "Please enter your name", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            
-            alert.addAction(okAction)
-            present(alert, animated: true, completion: nil)
-            
-            print("Name format is wrong")
-            
-        } else {
-            
-            label.text = textField.text
-            textField.text = nil
-        }
-    }
-    
-    @IBAction func changeDate(_ sender: UIDatePicker) {
-        
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateStyle = .medium
-        dateFormatter.locale = Locale(identifier: "ru_RU")
-        
-        let dateValue = dateFormatter.string(from: sender.date)
-        
-        label.text = dateValue
-    }
-    
-    @IBAction func switchAction(_ sender: UISwitch) {
-        
-        segmentedControl.isHidden = !segmentedControl.isHidden
-        label.isHidden = !label.isHidden
-        slider.isHidden = !slider.isHidden
-        textField.isHidden = !textField.isHidden
-        doneButton.isHidden = !doneButton.isHidden
-        datePicker.isHidden = !datePicker.isHidden
-        
-        if sender.isOn {
-            switchLabel.text = "Отобразить все элеметы"
-        } else {
-            switchLabel.text = "Скрыть все элементы"
-        }
-    }
     
 }
-
